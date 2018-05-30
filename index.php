713ed978ee9fbc22853a2694f289dd5932927c19
Anatolii Nehelia <a.negelya@windowslive.com>
2018-05-30 15:23:15 +0300

    firstTable function added

--- index.php
+++ index.php
@@ -1,89 +1,134 @@
 <?php
-
     require('PHPExcel.php');
 
     if(isset($_POST['submit'])){
-
+        // Отримуємо кулькість файлів
         $countfiles = count($_FILES['excel-file']['name']);
 
-        $flats = array();
-        $columns = array();
+        // Cтворюємо загальний асоціативний масив для квартир
+        $apparts = array();
         
+        // Для кожного з файлів здійснюємо парсинг
         for($l=0;$l<$countfiles;$l++){
             $target_dir = "uploads/";
             $target_file = $target_dir . basename($_FILES["excel-file"]["name"][$l]);
 
             if (move_uploaded_file($_FILES["excel-file"]["tmp_name"][$l], $target_file)) {
-                echo "Файл успішно завантажено.\n";
+                echo "<p>Файл успішно завантажено.\n</p>";
                 
                 $Reader = PHPExcel_IOFactory::createReaderForFile($target_file);
                 $Reader->setReadDataOnly(true);
                 $objXLS = $Reader->load($target_file);
                 $objWorksheet = $objXLS->getActiveSheet();
-            
+
+                $flat = null;
+
+                // Проходимось по рядкам таблиці
                 for ($i = 1; $i <= $objWorksheet->getHighestRow(); $i++) {  
                     
                     $nColumn = PHPExcel_Cell::columnIndexFromString(
                         $objWorksheet->getHighestColumn());
                     
+                    // Проходимось по колонках рядка таблиці
                     for ($j = 0; $j < $nColumn; $j++) {
                         $value = $objWorksheet->getCellByColumnAndRow($j, $i)->getValue();
-                        $flat;
-                        $explication;
 
-                        if($i !== 2) {
+                        // Пропускаємо пустий рядок і рядок із заголовками таблиці
+                        if($i > 2) {
                             if($j===0 && $value) {
+                                /* Якщо це перша клітинка в таблиці і вона не пуста,
+                                тобто відповідає базовому ID квартири,
+                                то перевіряємо її на відповідність регулярним виразам,
+                                зберігаємо її як поточну квартиру в локальну змінну та
+                                додаємо до загального масиву квартир.
+                                Також додаємо до нього масив приміщень,
+                                загальну площу квартири
+                                та масив поверхів, де розміщена квартира */
                                 if(preg_match('/^MZK/', $value) !== 1) {
                                     $flat = $value;
-                                    $flats[$value] = array();
+                                    $apparts[$value] = array();
+                                    $apparts[$flat]['rooms'] = array();
+                                    $apparts[$flat]['area'] = 0;
+                                    $apparts[$flat]['floor'] = array(); 
+                                } else {
+                                    // В іншому разі обнуляємо поточну квартиру
+                                    $flat = null;
                                 }
-                            } else if($j===2 && $value) {
-                                $flats[$flat][$value] = "";
-                                $explication = $value;
-                                if(!in_array($value, $columns)) {
-                                    $columns[] = $value;
-                                }
-                            } else if($j===3 && $value) {
-                                if(!$flats[$flat][$explication]) {
-                                    $flats[$flat][$explication] = $value;
+                            } else if($flat) {
+                                if($j===1 && $value) {
+                                    /* Якщо це третя клітинка в таблиці і вона не пуста,
+                                    тобто відповідає коду приміщення,
+                                    то зберігаємо її до масиву приміщень квартири,
+                                    туди додеємо ї назву, площу та поверх.
+                                    Площу та поверх кімнати також додаємо 
+                                    до загальної площі та масиву поверхів квартири */
+                                    $apparts[$flat]['rooms'][$value] = array();
+                                    $name = $objWorksheet->getCellByColumnAndRow($j+1, $i)->getValue();
+                                    $apparts[$flat]['rooms'][$value]['name'] = $name;
+    
+                                    $area = $objWorksheet->getCellByColumnAndRow($j+2, $i)->getValue();
+                                    $apparts[$flat]['area'] = $apparts[$flat]['area'] + $area;
+                                    $apparts[$flat]['rooms'][$value]['area'] = $area;
+    
+                                    $floor = $objWorksheet->getCellByColumnAndRow($j+3, $i)->getValue();
+                                    $apparts[$flat]['rooms'][$value]['floor'] = $floor;
+                                    if(!in_array($floor, $apparts[$flat]['floor'])) {
+                                        $apparts[$flat]['floor'][] = $floor;
+                                    }
                                 }
                             }
                         }
                     }
                 }
 
-                echo ("Файл створено та записано!\n".$l);
+                echo ("<p>Файл створено та записано!\n</p><p>Кількість файлів: ".($l+1)."</p>");
             } else {
-                echo "Можлива атака за допомогою файлів завантаження!\n";
+                echo "<p>Можлива атака за допомогою файлів завантаження!\n</p>";
             }
         }
 
-        sort($columns);
+        $firstTable = makeFirstTable($apparts);
 
-        $res = '"Квартира";';
-
-        foreach($columns as $col) {
-            $res .= '"'.$col .= '";';
+        // Записуємо у файл
+        $f = fopen("first_table.csv", "w");
+        foreach ($firstTable as $row) {
+            fputcsv($f, $row);
         }
-
-        $res .= "\r\n";
-
-        foreach($flats as $key => $flat) {
-            $res .= '"'.$key .= '";';
-            foreach($columns as $col) {
-                if($flat[$col]) {
-                    $res .= '"'.$flat[$col] .= '";';
-                } else {
-                    $res .= ";";
-                }
-            }
-
-            $res .= "\r\n";
-        }
-
-        $f = fopen("out.csv", "w"); 
-        fwrite($f, $res);
         fclose($f);
+    }
+
+    function makeFirstTable($apparts) {
+        $table = array(
+            array(
+                'Номер квартири',
+                'Тип',
+                'Секція',
+                'Під’їзд',
+                'Поверхи',
+                'Загальна площа',
+                'К-ть кімнат',
+                'Базовий ID'
+            )
+        );
+
+        foreach ($apparts as $flatId => $flat) {
+
+            preg_match('/^(\S+)\/(\S+)\/(\S+)\.(\S+)\_(\S+)\_(\S+)\_(\S+)/', $flatId, $idMatches);
+            $row = array(
+                $idMatches[2],
+                $idMatches[3].'.'.$idMatches[4],
+                $idMatches[5],
+                $idMatches[6],
+                implode(',', $flat['floor']),
+                $flat['area'],
+                $idMatches[3],
+                $flatId
+            );
+
+            $table[] = $row;
+        }
+
+        return $table;
     }
 ?>
 
